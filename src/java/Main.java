2c08e38e11b9309e8e6177338656a7b76d7ead47
v01dc0d3 <at200000000@gmail.com>
2021-05-26 17:46:01 +0200

    Added Screen Controller to control scenes

---
+++ src/java/Main.java
@@ -0,0 +1,19 @@
+package java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}

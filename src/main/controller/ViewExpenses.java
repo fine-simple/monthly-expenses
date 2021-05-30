c77c24cd25f9b6eec79c6600623760367eefa95c
Shehab <shehabs.910@gmail.com>
2021-05-30 21:37:44 +0200

    added loadAndDisplayTable(), tableview now displays real data from DAO

--- src/main/controller/ViewExpenses.java
+++ src/main/controller/ViewExpenses.java
@@ -1,13 +1,18 @@
 package main.controller;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import main.model.Expense;
 import main.model.dao.CategoryDao;
 import main.model.dao.ExpenseDao;
@@ -16,7 +21,7 @@ import main.model.dao.WalletDao;
 
 public class ViewExpenses implements Initializable {
 	@FXML
-	TableView<Expense> list;
+	TableView<Expense> table;
 
 	@FXML
 	ComboBox<String> month;
@@ -40,6 +45,8 @@ public class ViewExpenses implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		// Load All Date in the table
 
+		// Initialize Table
+		loadAndDisplayTable();
 		printAllDate();
 	}
 
@@ -69,5 +76,39 @@ public class ViewExpenses implements Initializable {
 		for (var entry : CategoryDao.getInstance().categories) {
 			System.out.println(entry);
 		}
+	}
+
+	private void loadAndDisplayTable() {
+		// Initialize Table
+        TableColumn<Expense, String> titleColumn = new TableColumn<>("Title");
+        titleColumn.setMinWidth(150);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+
+        TableColumn<Expense, String> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(51);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+
+        TableColumn<Expense, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setMinWidth(100);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+		TableColumn<Expense, String> categoryColumn = new TableColumn<>("Category");
+        categoryColumn.setMinWidth(100);
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+		TableColumn<Expense, String> walletColumn = new TableColumn<>("Wallet");
+        walletColumn.setMinWidth(75);
+        walletColumn.setCellValueFactory(new PropertyValueFactory<>("wallet"));
+
+		//get expenses
+		ObservableList<Expense> expenses = FXCollections.observableArrayList(ExpenseDao.getInstance().expenses);
+
+		//add expenses to table
+        table.setItems(expenses);
+		
+		//add columns to table
+        table.getColumns().addAll(titleColumn, priceColumn, dateColumn, categoryColumn, walletColumn);
 	}
 }

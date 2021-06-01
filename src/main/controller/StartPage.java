16f7eb292f4852a29584f94988a1a371dd9f07bd
v01dc0d3 <at200000000@gmail.com>
2021-06-01 15:40:30 +0200

    Added View Expenses Filters

--- src/main/controller/StartPage.java
+++ src/main/controller/StartPage.java
@@ -1,14 +1,12 @@
 package main.controller;
 
 import javafx.fxml.FXML;
-import main.debugging.debugging;
 
 public class StartPage {
     
     @FXML
     void start() {
         ScreenController.getInstance().activate("navigation");
-        debugging.addDebugData(150);
     }
     
     @FXML
